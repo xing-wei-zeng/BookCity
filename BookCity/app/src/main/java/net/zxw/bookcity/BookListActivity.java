90cf55b4b286a50188df06e3cfa367afcae7caba
zengxingwei <443075929@qq.com>
2018-04-18 17:58:11 +0800

    下载书籍

--- BookCity/app/src/main/java/net/zxw/bookcity/BookListActivity.java
+++ BookCity/app/src/main/java/net/zxw/bookcity/BookListActivity.java
@@ -1,9 +1,13 @@
 package net.zxw.bookcity;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,7 +22,9 @@ import android.widget.TextView;
 import com.google.gson.Gson;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.FileAsyncHttpResponseHandler;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,6 +38,7 @@ import cz.msebera.android.httpclient.Header;
 
 public class BookListActivity extends AppCompatActivity{
 
+    private AsyncHttpClient mClient;
     private static final String TAG = "zxw";
     private ListView mListView;
     List<BookListResult.Book> mBooks = new ArrayList<>();
@@ -43,8 +50,8 @@ public class BookListActivity extends AppCompatActivity{
         mListView = (ListView) findViewById(R.id.book_list_view);
         String url = "http://www.imooc.com/api/teacher?type=10";
 
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(url, new AsyncHttpResponseHandler() {
+        mClient = new AsyncHttpClient();
+        mClient.get(url, new AsyncHttpResponseHandler() {
 
             @Override
             public void onStart() {
@@ -98,7 +105,7 @@ public class BookListActivity extends AppCompatActivity{
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            BookListResult.Book book = mBooks.get(position);
+            final BookListResult.Book book = mBooks.get(position);
 
             ViewHolder viewHolder = new ViewHolder();
             if(convertView == null){
@@ -111,6 +118,58 @@ public class BookListActivity extends AppCompatActivity{
             }
 
             viewHolder.mNameTextView.setText(book.getBookname());
+            final String path = Environment.getExternalStorageDirectory()+"/mnt/"+book.getBookname()+".txt";
+            final File file = new File(path);
+            viewHolder.mButton.setText(file.exists()? R.string.download_success:R.string.button_onclick);
+
+            final ViewHolder finalViewHolder = viewHolder;
+            viewHolder.mButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int REQUEST_EXTERNAL_STORAGE = 1;
+                    String[] PERMISSIONS_STORAGE = {
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    };
+                    int permission = ActivityCompat.checkSelfPermission(BookListActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+                    if (permission != PackageManager.PERMISSION_GRANTED) {
+                        // We don't have permission so prompt the user
+                        ActivityCompat.requestPermissions(
+                                BookListActivity.this,
+                                PERMISSIONS_STORAGE,
+                                REQUEST_EXTERNAL_STORAGE
+                        );
+                    }
+                    //下载
+
+                    if(file.exists()){
+                        //打开书籍
+
+                    }else {
+                        mClient.addHeader("Accept-Encoding", "identity");
+                        mClient.get(book.getBookfile(), new FileAsyncHttpResponseHandler(file) {
+                            @Override
+                            public void onFailure(int statusCode, Header[] headers, Throwable throwable, File file) {
+                                finalViewHolder.mButton.setText(R.string.download_failure);
+                            }
+
+                            @Override
+                            public void onSuccess(int statusCode, Header[] headers, File file) {
+                                finalViewHolder.mButton.setText(R.string.download_success);
+                            }
+
+                            @Override
+                            public void onProgress(long bytesWritten, long totalSize) {
+                                super.onProgress(bytesWritten, totalSize);
+
+                                finalViewHolder.mButton.setText(String.valueOf(bytesWritten * 100 / totalSize) + "%");
+                            }
+                        });
+                    }
+
+                }
+            });
             return convertView;
         }
         class ViewHolder{
