35845771146af774f28e892f1d96e8a7d8486a1c
zengxingwei <443075929@qq.com>
2018-04-18 17:12:18 +0800

    书籍列表显示

--- BookCity/app/src/main/java/net/zxw/bookcity/BookListActivity.java
+++ BookCity/app/src/main/java/net/zxw/bookcity/BookListActivity.java
@@ -6,11 +6,20 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
@@ -24,11 +33,14 @@ import cz.msebera.android.httpclient.Header;
 public class BookListActivity extends AppCompatActivity{
 
     private static final String TAG = "zxw";
+    private ListView mListView;
+    List<BookListResult.Book> mBooks = new ArrayList<>();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
 
+        mListView = (ListView) findViewById(R.id.book_list_view);
         String url = "http://www.imooc.com/api/teacher?type=10";
 
         AsyncHttpClient client = new AsyncHttpClient();
@@ -46,7 +58,9 @@ public class BookListActivity extends AppCompatActivity{
                 Gson gson = new Gson();
                 BookListResult bookListResult = gson.fromJson(result,BookListResult.class);
 
-                List<BookListResult.Book> books = bookListResult.getMData();
+                mBooks = bookListResult.getMData();
+
+                mListView.setAdapter(new BookListAdapter());
             }
 
             @Override
@@ -64,5 +78,44 @@ public class BookListActivity extends AppCompatActivity{
     public static void start(Context context){
         Intent intent = new Intent(context,BookListActivity.class);
         context.startActivity(intent);
+    }
+
+    private class BookListAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return mBooks.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mBooks.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            BookListResult.Book book = mBooks.get(position);
+
+            ViewHolder viewHolder = new ViewHolder();
+            if(convertView == null){
+                convertView = getLayoutInflater().inflate(R.layout.item_book_list_view,null);
+                viewHolder.mNameTextView = (TextView) convertView.findViewById(R.id.name_text_view);
+                viewHolder.mButton = (Button) convertView.findViewById(R.id.book_button);
+                convertView.setTag(viewHolder);
+            }else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+
+            viewHolder.mNameTextView.setText(book.getBookname());
+            return convertView;
+        }
+        class ViewHolder{
+            public TextView mNameTextView;
+            public Button mButton;
+        }
     }
 }
